cf43304fac3f0456e8bef14700828844545048f3
Darkc0de <87938019+1madtanker1@users.noreply.github.com>
2021-12-31 19:41:17 -0400

    Create hacker2.py

---
+++ hacker2.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import time
+import random
+import subprocess
+import random
+import threading
+import socket
+
+
+a = """
+
+NmNMNmNMNNMMNNMMmNMMmNMNmNMNmNMNmNMNmNMNmNMNmNMmmNMNNMMNNMMmNMNmmMNmNMNmNMNmNMNmNMNmNMNmNMNNMMNNMMNN
+NMNmNMNmMMNmMMNNMMmNMMmNMNmNMNmNMNmNNmhhhysssssosysyhddNNmNMMmdyshmMNmNMNmNMNmNMNmNMNmNMNmNMNmMMNNMM
+NmNMNmNMNmMMNmMMmNMMmNMMmNMNmNMNdyyo++/+++/++//++//++//+osyddy+++/odNMNmNMmysyyymMNmNMNmNMNmMMNmMMmN
+NMNmNMNmMMNmMMmmMMmNMMmNMMmNNhs+/+++/+++/++//++//++//++//++/+++/+++/odNNo-......./yMNmNMNmNMNmMMmmMM
+NmNMNmMMNmMMNmMMmmMMmNMMmmho++//++/+++/+++/+++/+++/+++/+++/++//++//++/o-...........:hMNmNMNmMMNmMMmm
+NMNmNMNmMMNmMMNmMMmmMMmdy+++//++/+++/+++/+++/+++/+++/+++/++//++//++//++/:-/hmmmo-..../hMNmNMNmMMNmMM
+NmNMNmNMNmMMNmMMNmMMNds+++//++/+++/+++/+++/+++/+++/+++/++//++//++//++//++/sdNMNmmo-..../hMNmNMNmMMNm
+NMNmNMNmNMNmNMNmMMNmy+++//++/+++/+++/+++/+++/+++/+++/++//++//++//++//++/++++ydNMNmd+....-/dMNmNMNmMM
+NmNMNmNMNmNMNmNMNmmo++//++/+++/+++/+++/+++/+++/+++/++//++//++//++//++/+++/++++ydNMNmd+....-+dMNmNMNm
+NMNmNMNmNMNmNMNmNd++//++/+++/+++/+++/+++/+++/+++/++//++//++//++//++/+++/+++/+++smmNMNmd/...-omNMNmNM
+NmNMNmNMNmNMNmNMh+//++//++//++//++//++//++//++/++//++//++//++//++//++//++//++oymNMNmNMNmh/+hNMNmNMNm
+NMNmNMNmNMNmNMNh+/+++/+++/++//++//++//++//++/+++/+++/+++/+++/+++/+++/++//++/+dNMNmNMNmNMNmNMNmNMNmNM
+NmNMNmNMNmNMNmm++++/+++/++//++//++//++//++/+++/+++/+++/+++/+++/+++/++//++//++yNmNMNmNMNmNMNmNMNmNMNm
+NMNmNMNmNMNmNMs++/+++/++//++//++//++//++/+++/+++/+++/+++/+++/+++/++//++//++//+dMNmNMNmNMNmNMNmNMNmNM
+NmNMNmNMNmNMNh+/+++/++/:------://+//-------:///+++/+++/+++/+++//:--:///++//++/omNMNmNMNmNMNmNMNmNMNm
+NMNmNMNmNMNmNs+++/++//+-        ./+/        `-/+/+++/+++/+++//`     `:+//++//++mMmNMNmNMNmNMNmNMNmNM
+MmNMNmNMNmNMN++/++//++/.  .:::-   //   ::::`  .+++/+++/+++/+/`  -:-   /++//++/+yNMMmNMNmNMNmNMNmNMNm
+NMNmNMNmNMNmd/+++/+++/+-  .++/+`  :/   ///+:  `+//-.`  `.:++/`  -:///+++/++//++sMmNMNmNMNmNMNmNMNmNM
+NmNMNmNMNmNMd++/+++/+++.  ./+++`  -/   /++/:  `//`  `..`  ./+:`     .//++//++//sNMMmNMNmNMNmNMNmNMNm
+NMNmNMNmNMNmd/+++/+++/+-  .++/+`  :/   ///+:  `+-  .////`  :/+//:-.  `///++//++sMmNMNmNMNmNMNmNMNmNM
+NmNMNmNMNmNMm++/+++/+++.  .///:   :/   ////.  .+:  `://:   :/.``:/:`  :++//++//sNMMNNMNmNMNmNMNmNMNm
+NMNmNMNmNMNmNo+++/+++/+-   ```  `-+/   ````  ./+/-`  ``  `-//-`  ``  .///++//++dMNNMNNNMNmNMNmNMNmNM
+MNNMNNNMNmNMNy+/+++/+++-.......:/+//.......-://++//-....://+++/-...-://++//++/+mNMMNNMNNNMNmNMNmNMNm
+NMNNNMNmNMNmNN+++//++/++//++//++//++//++//++//++//++//++//++//+//++//++//++//+yNMNNMMNNMNNNMNmNMNmNM
+MNNMMNNMNmNMNmh+/++/+++/+++/+++/+++/+++/+++/++//++//++//++//++/+++/+++/+++/++oNNNNMNNMMNNMNmNMNmNMNm
+NMMNNMMmNMNmNMNs+/+++/+++/+++/+++/+++/+++/++//++//++//++//++/+++/+++/+++/++++dmNMNNMMNNMMNNMNmNMNmNM
+MNNMMNNMMmNMNmNNs++/+++/+++/+++/+++/+++/++//++//++//++//++/+++/+++/+++/+++/+hNMNmNMNNMMNNMMmNMMmNMNm
+mMMNNMMmNMMmNMMmms+++/+++/+++/+++/+++/++//++//++//++//++/+++/+++/+++/+++/++dMNmNMNmMMNmMMNNMMmNMMmNM
+MNmMMmmMMmNMMmNMMmy++++/+++/+++/+++/++//++//++//++//++/+++/+++/+++/+++/++omNmNMNmNMNmMMNmMMmNMMmNMMm
+mMMNmMMmmMMmNMMmNMMho/+++/+++/+++/++//++//++//++//++/+++/+++/+++/+++/+++ymmNMNmNMNmMMNmMMmmMMmNMMmNM
+MNmMMNmMMmmMMmNMMmNMNy+//++/+++/+++/+++/+++/+++/++//++//++//++//++/+++sdmNMNmNMNmMMNmMMNmMMmmMMmNMMm
+mMMNmMMNmMMmNMMmNMNmNMNy+/+++/+++/+++/+++/+++/++//++//++//++//++/+++sdmNMNmNMNmNMNmMMNmMMNmMMmmMMmNM
+MNmMMNmMMNNMMmNMMmNMNmNMNys++++/+++/+++/+++/++//++//++//++//++/+++ydmNMNmNMNmNMNmNMNmMMNmMMNmMMNNMMm
+mNMNmMMNmMMNNMMNNMNmNMNmNMNdho+++/+++/+++/++//++//++//++//++/+osdmmNMNmNMNmNMNmNMNmNMNmNMNmMMNNMMNNM
+MNmNMNmNMNNMMNNMMNNMNmNMNmNMNmmdso+++++/++//++//++//++//++ooydNNmNMNmNMNmNMNmNMNmNMNmNMNmNMNmMMNNMMN
+mNMNmNMNmNMNNMMNNMNNNMNmNMNmNMNmNNmddyssoo+++++++++oooshdmmmMNNNMNmNMNmNMNmNMNmNMNmNMNmNMNmNMNNMMNNM
+MNmNMNmNMNNNMNNMMNNMNNNMNmNMNmNMNmNMNmNMmdmmddmmddNNmmNMNNMMNNMNNNMNmNMNmNMNmNMNmNMNmNMNmNMNmNMNNNMN
+
+"""
+print(a)
+print("Here are your options:")
+print("1 = Preform a Wifi scan to find network devices")
+print("2 - Scan a port")
+print("3 - Read credits")
+
+num = input("Enter your chosen option > ")
+#DDOS ATTACK
+#NETWORK SCAN
+elif int(num) == 1:
+      os.system('clear')
+      os.system('sudo python3 wifiscan.py')
+      
+elif int(num) == 2:
+      os.system('clear')
+      os.system('sudo python3 portscanner.py')
+
+elif int(num) == 3:
+      os.system('vim credits.txt')
+      
+else:
+    print("You entered an invalid number, please restart the script")
+
