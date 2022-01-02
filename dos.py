4e1d93819dd64cd623d27e3e3f98331a13e005ec
Darkc0de <87938019+1madtanker1@users.noreply.github.com>
2022-01-02 00:24:05 -0400

    Update dos.py

--- dos.py
+++ dos.py
@@ -13,7 +13,7 @@ print(ascii_banner)
 a = input("Enter an IP to attack > ")
 ip = (a)
 port = int(input("Enter the port to attack > "))
-duration = int(input("How long should Bomb send packets (how many seconds)? > "))
+duration = int(input("How long should NET send packets (how many seconds)? > "))
 sent = 0
 timeout = time.time() + duration
 
